b9d958aeac7dc71db6bb8db7735155d295085dee
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-09-13 13:42:28 -0600

    end-proyect

--- src/controllers/notes.controllers.js
+++ src/controllers/notes.controllers.js
@@ -9,18 +9,24 @@ notesCtrl.renderNotesFrom = (req, res) => {
 notesCtrl.createNewNotes = async (req, res) => {
     const { title, description } = req.body
     const newNote = new Notes({title, description})
+    newNote.user = req.user.id
     await newNote.save()
     req.flash('success_message', 'notes added Succefuly')
     res.redirect('/notes')
 }
 
 notesCtrl.renderNotes = async (req, res) => {
-    const notes = await Notes.find()
+    const notes = await Notes.find({user: req.user.id})
     res.render('notes/all-notes', {notes})
 }
 
 notesCtrl.renderEditForm = async (req, res) => {
     const note = await Notes.findById(req.params.id)
+    console.log(req.user.id)
+    if(note.user != req.user.id) {
+        req.flash('error', 'No authorized')
+        return res.redirect('/notes/')
+    }
     res.render('notes/edit-notes', { note })
 }
 

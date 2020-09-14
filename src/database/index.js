ebd5d8349e9293b64bd31e1f3d4ae9bf5297061b
Miguel Hernández Gaitan <60478285+MiguelHG2351@users.noreply.github.com>
2020-09-13 22:40:48 -0600

    mongodb atlas added

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env
-const MONGOBD_URI = `mongodb://${NOTES_APP_MONGODB_HOST }/${ NOTES_APP_MONGODB_DATABASE }`
+const MONGOBD_URI = `mongodb+srv://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`
 
 console.log(MONGOBD_URI)
 mongoose.connect(MONGOBD_URI, {
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useFindAndModify: false
 })
 
 .then(db => console.log('la base de datos ha sido actualizada xd'))
